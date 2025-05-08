fef7c33022a642d0ff839ee773b1a3fcd3eca36e
woonwoo <qufgkswkfl3@gmail.com>
2025-05-09 03:33:57 +0900

    refactor: cloudinary configuration dotenv

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,12 +1,7 @@
 const { v2: cloudinary } = require("cloudinary");
-const dotenv = require("dotenv");
-dotenv.config();
-
-console.log("CLOUDINARY ENV:", {
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET ? "loaded" : "undefined",
-});
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
