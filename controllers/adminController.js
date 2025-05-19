73206e797b949c411f8b0c4659a3aac9a25363d4
woonwoo <qufgkswkfl3@gmail.com>
2025-05-19 17:14:04 +0900

    feat(admin): add getAllUsers controller with optional fields

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,5 @@
 const redis = require("../config/redis");
+const User = require("../models/User");
 
 const clearCache = async (req, res) => {
 	try {
@@ -10,4 +11,16 @@ const clearCache = async (req, res) => {
 	}
 };
 
-module.exports = { clearCache };
+const getAllUsers = async (req, res) => {
+	try {
+		const { fields } = req.query;
+		const selectFields = fields ? fields.split(",").join(" ") : null;
+		const users = await User.find().select(selectFields);
+		res.status(200).json(users);
+	} catch (err) {
+		console.error("유저 목록 조회 실패:", err);
+		res.status(500).json({ message: "유저 목록 조회 실패" });
+	}
+};
+
+module.exports = { clearCache, getAllUsers };
--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const requireAdmin = require("../middlewares/requireAdmin");
 
-const { clearCache } = require("../controllers/adminController");
+const { clearCache, getAllUsers } = require("../controllers/adminController");
 
 router.post("/clear-cache", requireAdmin, clearCache);
+router.get("/getAllUsers", requireAdmin, getAllUsers);
 
 module.exports = router;
