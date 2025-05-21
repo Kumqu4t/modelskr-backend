4f618a6d670b7e083113493c4a1259bccd2fef05
woonwoo <qufgkswkfl3@gmail.com>
2025-05-21 15:19:00 +0900

    fix(utils): apply select() properly in paginateQuery

--- utils/paginateQuery.js
+++ utils/paginateQuery.js
@@ -1,6 +1,6 @@
 const paginateQuery = ({ model, filter, page = 1, limit = 16, fields }) => {
 	const skip = (page - 1) * limit;
-	const query = model.find(filter, fields).skip(skip).limit(limit);
+	const query = model.find(filter).select(fields).skip(skip).limit(limit);
 	const countQuery = model.countDocuments(filter);
 
 	return { query, countQuery };
